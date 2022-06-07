f5914e6e125b3d0e461ff99431880aa3899bc2ad
Sakib Sobhan Khan <96111822+sakibsobhan@users.noreply.github.com>
2022-06-07 18:46:43 +0600

    MK Task2

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// banner slider
+$(".banner-slider").slick({
+    prevArrow:'<i class="fa-solid fa-angle-left prev"></i>',
+    nextArrow:'<i class="fa-solid fa-angle-right next"></i>',
+    autoplay: true,
+})
+
+// aos animation
+AOS.init();
